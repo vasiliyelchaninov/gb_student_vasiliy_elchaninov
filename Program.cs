d2b0855940c1e3131b6af664c7eb9c9aab4d7cde
vasiliyelchaninov <115776859+vasiliyelchaninov@users.noreply.github.com>
2022-11-24 18:14:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,51 +1,137 @@
-﻿//Задача 10
-// Напишите программу, которая принимает на вход трёхзначное число
-// и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
 
-Console.Clear();
-Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+/*Console.WriteLine("Задача 19")
+Console.Write("Введите число: ")
+string? number = Console.ReadLine()
+void CheckingNumber(string number)
+if (number[0]==number[4] || number[1]==number[3])
+Console.WriteLine($"Ваше число: {number} - палиндром.")
+Console.WriteLine($"Ваше число: {number} - НЕ палиндром.")
+if (number!.Length == 5)
+CheckingNumber(number)
+Console.WriteLine($"Введи правильное число")
+*/
 
-// Задача 13
-// Напишите программу, которая выводит третью цифру
-// заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+Console.WriteLine("Задача 19");
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
 
-Console.Write("Введи число: ");
-int anyNumber = Convert.ToInt32(Console.ReadLine());
-string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
-  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+void CheckingNumber(string number){
+if (number[0]==number[4] || number[1]==number[3]){
+Console.WriteLine($"Ваше число: {number} - палиндром.");
 }
-else {
-  Console.WriteLine("-> третьей цифры нет");
-}
-
-//Задача 15
-// Напишите программу, которая принимает на вход цифру,
-// обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-
-Console.Write("Введи цифру, обозначающую день недели: ");
-int dayNumber = Convert.ToInt32(Console.ReadLine());
-
-void CheckingTheDayOfTheWeek (int dayNumber) {
-  if (dayNumber == 6 || dayNumber == 7) {
-  Console.WriteLine("(этот день выходной) -> да");
-  }
-  else if (dayNumber < 1 || dayNumber > 7) {
-    Console.WriteLine("это вообще не день недели");
-  }
-  else Console.WriteLine("(этот день не выходной) -> нет");
+else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
 }
 
-CheckingTheDayOfTheWeek(dayNumber);
+if (number!.Length == 5){
+CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/*Console.WriteLine("\n Задача 21")
+int x1 = Coordinate("x", "A")
+int y1 = Coordinate("y", "A")
+int z1 = Coordinate("z", "A")
+int x2 = Coordinate("x", "B")
+int y2 = Coordinate("y", "B")
+int z2 = Coordinate("z", "B")
+int Coordinate(string coorName, string pointName)
+Console.Write($"Введите координату {coorName} точки {pointName}: ")
+return Convert.ToInt16(Console.ReadLine())
+double Decision(double x1, double x2, 
+double y1, double y2, 
+double z1, double z2)
+return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+Math.Pow((y2-y1), 2) + 
+Math.Pow((z2-z1), 2))
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 )
+Console.WriteLine($"Длина отрезка  {segmentLength}")
+*/
+
+Console.WriteLine("\n Задача 21");
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+Console.Write($"Введите координату {coorName} точки {pointName}: ");
+return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+double y1, double y2, 
+double z1, double z2){
+return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+Math.Pow((y2-y1), 2) + 
+Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
+
+
+//Задача 23: Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+/*Console.WriteLine("\n Задача 23")
+Console.Write("Введите число: ")
+int cube = Convert.ToInt32(Console.ReadLine())
+void Cube(int[] cube)
+int counter = 0
+int length = cube.Length
+while (counter <  length)
+cube[counter] = Convert.ToInt32(Math.Pow(counter, 3))
+counter++
+void PrintArry(int[] coll)
+int count = coll.Length
+int index = 0
+while(index < count){
+Console.Write(coll[index]+ " ")
+index++
+int[] arry = new int[cube+1]
+Cube(arry)
+PrintArry(arry)
+*/
+
+Console.WriteLine("\n Задача 23");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+int counter = 0;
+int length = cube.Length;
+while (counter <  length){
+cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+counter++;
+}
+}
+
+void PrintArry(int[] coll){
+int count = coll.Length;
+int index = 0;
+while(index < count){
+Console.Write(coll[index]+ " ");
+index++;
+}
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
