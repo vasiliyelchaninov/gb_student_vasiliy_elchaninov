22e16b98448760c8d16c95f8db86f0cdca28dcda
vasiliyelchaninov <115776859+vasiliyelchaninov@users.noreply.github.com>
2022-11-29 09:54:27 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,137 +1,55 @@
-﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число
-// и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 23432 -> да
-// 12821 -> да
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
 
-/*Console.WriteLine("Задача 19")
-Console.Write("Введите число: ")
-string? number = Console.ReadLine()
-void CheckingNumber(string number)
-if (number[0]==number[4] || number[1]==number[3])
-Console.WriteLine($"Ваше число: {number} - палиндром.")
-Console.WriteLine($"Ваше число: {number} - НЕ палиндром.")
-if (number!.Length == 5)
-CheckingNumber(number)
-Console.WriteLine($"Введи правильное число")
-*/
+Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
 
-Console.WriteLine("Задача 19");
-Console.Write("Введите число: ");
-string? number = Console.ReadLine();
-
-void CheckingNumber(string number){
-if (number[0]==number[4] || number[1]==number[3]){
-Console.WriteLine($"Ваше число: {number} - палиндром.");
-}
-else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+int Exponentiation(int numberA, int numberB){
+int result = 1;
+for(int i=1; i <= numberB; i++){
+result = result * numberA;
+  }
+// int result = Math.Pow(numberA, numberB);
+return result;
 }
 
-if (number!.Length == 5){
-CheckingNumber(number);
-}
-else Console.WriteLine($"Введи правильное число");
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ: " + exponentiation);
 
 
-// Задача 21: Напишите программу, которая принимает на вход координаты двух точек
-// и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-/*Console.WriteLine("\n Задача 21")
-int x1 = Coordinate("x", "A")
-int y1 = Coordinate("y", "A")
-int z1 = Coordinate("z", "A")
-int x2 = Coordinate("x", "B")
-int y2 = Coordinate("y", "B")
-int z2 = Coordinate("z", "B")
-int Coordinate(string coorName, string pointName)
-Console.Write($"Введите координату {coorName} точки {pointName}: ")
-return Convert.ToInt16(Console.ReadLine())
-double Decision(double x1, double x2, 
-double y1, double y2, 
-double z1, double z2)
-return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-Math.Pow((y2-y1), 2) + 
-Math.Pow((z2-z1), 2))
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 )
-Console.WriteLine($"Длина отрезка  {segmentLength}")
-*/
+Console.WriteLine($"\nЗадача 27. Выдаёт сумму цифр в числе");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("\n Задача 21");
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+int SumNumber(int numberN){
+    
+int counter = Convert.ToString(numberN).Length;
+int advance = 0;
+int result = 0;
 
-int Coordinate(string coorName, string pointName)
-{
-Console.Write($"Введите координату {coorName} точки {pointName}: ");
-return Convert.ToInt16(Console.ReadLine());
+for (int i = 0; i < counter; i++){
+advance = numberN - numberN % 10;
+result = result + (numberN - advance);
+numberN = numberN / 10;    }
+return result;
 }
 
-double Decision(double x1, double x2, 
-double y1, double y2, 
-double z1, double z2){
-return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-Math.Pow((y2-y1), 2) + 
-Math.Pow((z2-z1), 2));
-}
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
 
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+// Задача 29: Напишите программу, которая задаёт массив из
+// 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine($"Длина отрезка  {segmentLength}");
-
-
-//Задача 23: Напишите программу, которая принимает на вход
-// число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
-
-/*Console.WriteLine("\n Задача 23")
-Console.Write("Введите число: ")
-int cube = Convert.ToInt32(Console.ReadLine())
-void Cube(int[] cube)
-int counter = 0
-int length = cube.Length
-while (counter <  length)
-cube[counter] = Convert.ToInt32(Math.Pow(counter, 3))
-counter++
-void PrintArry(int[] coll)
-int count = coll.Length
-int index = 0
-while(index < count){
-Console.Write(coll[index]+ " ")
-index++
-int[] arry = new int[cube+1]
-Cube(arry)
-PrintArry(arry)
-*/
-
-Console.WriteLine("\n Задача 23");
-Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
-
-void Cube(int[] cube){
-int counter = 0;
-int length = cube.Length;
-while (counter <  length){
-cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-counter++;
-}
-}
-
-void PrintArry(int[] coll){
-int count = coll.Length;
-int index = 0;
-while(index < count){
-Console.Write(coll[index]+ " ");
-index++;
-}
-} 
-
-int[] arry = new int[cube+1];
-Cube(arry);
-PrintArry(arry);
