5ffb8a58a12d8c296c70923cf6297aac851166c6
vasiliyelchaninov <115776859+vasiliyelchaninov@users.noreply.github.com>
2022-12-16 09:34:05 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,199 +1,108 @@
-﻿//Задача 54:
-/*Задайте двумерный массив. Напишите программу,
-которая упорядочит по убыванию элементы каждой строки двумерного массива.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2
+﻿//Задача 64:
+/*Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+ 
+Console.Write($"\nЗадача 64: Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
 
 
-internal class Program
+void NaturalToLow(int n, int count)
 {
-    private static void Main(string[] args)
+    if (count > n)
     {
-        Console.WriteLine("Задача 54. Введите количество строк");
-        int linesVol = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("введите количество столбцов");
-        int columnsVol = Convert.ToInt32(Console.ReadLine());
-        int[,] numbers = new int[linesVol, columnsVol];
-        FillArrayRandomNumbers(numbers);
-        Console.WriteLine();
-        Console.WriteLine("Массив до изменения");
-        PrintArray(numbers);
-
-        for (int i = 0; i < numbers.GetLength(0); i++)
-        {
-            for (int j = 0; j < numbers.GetLength(1) - 1; j++)
-            {
-                for (int z = 0; z < numbers.GetLength(1) - 1; z++)
-                {
-                    if (numbers[i, z] < numbers[i, z + 1])
-                    {
-                        int temp = 0;
-                        temp = numbers[i, z];
-                        numbers[i, z] = numbers[i, z + 1];
-                        numbers[i, z + 1] = temp;
-                    }
-                }
-            }
-        }
-        Console.WriteLine();
-        Console.WriteLine("Массив с упорядоченными значениями");
-        PrintArray(numbers);
-
-        void FillArrayRandomNumbers(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(0, 10);
-                }
-            }
-        }
-
-        void PrintArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                Console.Write("[ ");
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.Write("]");
-                Console.WriteLine("");
-            }
-        }
-
-        //Задача 56: 
-        /*Задайте прямоугольный двумерный массив. Напишите программу,
-        которая будет находить строку с наименьшей суммой элементов.
-        Например, задан массив:
-        1 4 7 2
-        5 9 2 3
-        8 4 2 4
-        5 2 6 7
-        Программа считает сумму элементов в каждой строке и выдаёт номер строки
-        с наименьшей суммой элементов: 1 строка
-        */
-
-        Console.WriteLine("Задача 56. Введите размер массива m x n и диапазон случайных значений:");
-        int m = InputNumbers("Введите m: ");
-        int n = InputNumbers("Введите n: ");
-        int range = InputNumbers("Введите диапазон: от 1 до ");
-
-        int[,] array = new int[m, n];
-        CreateArray(array);
-        WriteArray(array);
-
-        int minSumLine = 0;
-        int sumLine = SumLineElements(array, 0);
-        for (int i = 1; i < array.GetLength(0); i++)
-        {
-            int tempSumLine = SumLineElements(array, i);
-            if (sumLine > tempSumLine)
-            {
-                sumLine = tempSumLine;
-                minSumLine = i;
-            }
-        }
-
-        Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
-
-
-        int SumLineElements(int[,] array, int i)
-        {
-            int sumLine = array[i, 0];
-            for (int j = 1; j < array.GetLength(1); j++)
-            {
-                sumLine += array[i, j];
-            }
-            return sumLine;
-        }
-
-        int InputNumbers(string input)
-        {
-            Console.Write(input);
-            int output = Convert.ToInt32(Console.ReadLine());
-            return output;
-        }
-
-        void CreateArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(range);
-                }
-            }
-        }
-
-        void WriteArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-
-        //Задача 62. 
-        /*Напишите программу, которая заполнит спирально массив 4 на 4.
-        Например, на выходе получается вот такой массив:
-        01 02 03 04
-        12 13 14 05
-        11 16 15 06
-        10 09 08 07
-        */
-
-        Console.WriteLine("Задача 62. Введите размер массива");
-        int size = Convert.ToInt32(Console.ReadLine());
-
-        int[,] nums = new int[size, size];
-
-        int num = 1;
-        int i = 0;
-        int j = 0;
-
-        while (num <= size * size)
-        {
-            nums[i, j] = num;
-            if (i <= j + 1 && i + j < size - 1)
-                ++j;
-            else if (i < j && i + j >= size - 1)
-                ++i;
-            else if (i >= j && i + j > size - 1)
-                --j;
-            else
-                --i;
-            ++num;
-        }
-
-        PrintArray(nums);
+        return;
     }
-        void PrintArray(int[,] array)
+    else
     {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            Console.Write("[ ");
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                Console.Write(array[i, j] + " ");
-            }
-            Console.Write("]");
-            Console.WriteLine("");
-        }
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
     }
 }
+
+
+
+//Задача 66:
+/*Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+ 
+
+Console.Write($"\nЗадача 66: Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+
+
+//Задача 68:
+/* Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+ 
+Console.Write($"\nЗадача 68. Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+
+
 
